da1b3f7ed9d011c75d53c60db3fb8302431d5938
Lucas Ezequiel Malczewski <malczewskicukla@gmail.com>
2022-08-03 15:31:45 -0300

    Actualizacion del espacio de trabajo, agregando todo. nuevo formato

--- pracicas/tictactoe/js/index.js
+++ pracicas/tictactoe/js/index.js
@@ -1,49 +1,13 @@
 let turnCount = 1;
 let turnContainer="x";
+let rivalSelect = "player";
+let dama = false;
+let turnContainerAux = "";
+let enTuCara = "no";
 const playerOne = [];
 const playerTwo = [];
 const playerCpu = [];
 const celdas = document.querySelectorAll(".celdas");
-celdas.forEach(elem=>{
-    elem.addEventListener("click",()=>{
-        if(elem.innerHTML === ""){
-            if(turnPlace()===`<div id="x"></div>`){
-                elem.innerHTML = `<div id="x"></div>`;
-                playerOne.push(Number(elem.id));
-                anyWon();
-                cpu();
-            }else{
-                elem.innerHTML = `<div id="o"></div>`;
-                playerTwo.push(Number(elem.id));
-                anyWon();
-                cpu();
-            }
-        }
-    })
-})
-document.querySelectorAll(".turn").forEach(elem=>{
-    elem.addEventListener("click",()=>{
-        turnContainer = elem.id;
-        celdas.forEach(elem=>{
-            elem.innerHTML = ""
-        })
-        playerOne.length=0;
-        playerTwo.length=0;
-        playerCpu.length=0;
-        turnCount = 1;
-        if(turnContainer==="o"){
-            turnCount=3;
-            cpu();
-        }
-    })
-})
-const turnPlace = ()=>{
-    if(turnContainer==="o" && turnCount == 1){
-        turnCount++;
-    }
-    turnCount++;
-    return turnCount % 2 === 0 ? `<div id="x"></div>` : `<div id="o"></div>`
-}
 const winCondition = [
     [0,1,2],
     [3,4,5],
@@ -54,17 +18,19 @@ const winCondition = [
     [0,4,8],
     [2,4,6]
 ];
-const anyWon = ()=>{
-    winCondition.forEach(arr=>{
-        if(arr.every(elem=>playerOne.includes(elem))){
-            console.log("player one Win");
-        }else if(arr.every(elem=>playerTwo.includes(elem))){
-            console.log("player two Win");
-        }
-    })
+//------------------------------Funciones necesarias para los eventos------------------------------
+//Encargado de colocar X - O segun corresponda
+const turnPlace = ()=>{
+    if(turnContainer==="o" && turnCount == 1){
+        turnCount++;
+    }
+    turnCount++;
+    turnIndicator(turnCount);
+    return turnCount % 2 === 0 ? `<div class="animacion-x" id="x"></div>` : `<div class="animacion-o" id="o"></div>`
 }
+//Esto maneja las estategias de la computadora
 const cpu = ()=>{
-    /* Si la cpu va primero */
+    let gameCover = "short";
     try {
         /* paso uno */
         if(celdas[4].innerHTML === ""){
@@ -73,6 +39,30 @@ const cpu = ()=>{
             throw cpuPlayed;
         }
         /* paso dos */
+        winCondition.forEach(arr=>{
+            let playerCount = 0;
+            let playerAux = 4;
+            arr.map(elem=>{
+                if(playerOne.includes(elem) || playerTwo.includes(elem)){
+                    playerCount++;
+                }else{
+                    playerAux = elem;
+                }
+                if(playerCount>1 && celdas[playerAux].innerHTML === "" && Math.random()*100<20){
+                    if(turnPlace() == `<div class="animacion-x" id="x"></div>`){
+                        celdas[playerAux].innerHTML = `<div class="animacion-x-o" id="xo"></div>`;
+                    }
+                    else{
+                        celdas[playerAux].innerHTML = `<div class="animacion-o-x" id="ox"></div>`;
+                    }
+                    playerCpu.length = 0;
+                    playerCpu.push(...arr);
+                    gameCover = "long";
+                    enTuCara = "yes"
+                    throw cpuPlayed;
+                }
+            })
+        })
         /* paso tres */
         winCondition.forEach(arr=>{
             let cpuCount = 0;
@@ -83,7 +73,6 @@ const cpu = ()=>{
                 }else{
                     cpuAux = elem;
                 }
-                console.log(`contador=${cpuCount}   numeroDelElemento=${cpuAux}   comparacionEnElIf=${cpuCount>1 && celdas[cpuAux].innerHTML === ""}`)
                 if(cpuCount>1 && celdas[cpuAux].innerHTML === ""){
                     celdas[cpuAux].innerHTML = turnPlace();
                     playerCpu.push(cpuAux);
@@ -110,32 +99,26 @@ const cpu = ()=>{
         /* cuanto paso */
         /* Estrategias */
         /* Si el otro jugador puso en el centro, cpu coloca en una esquina */
-        console.log("1")
         if(playerOne.includes(4) || playerTwo.includes(4)){
-            console.log("2")
             if(celdas[0].innerHTML === ""){
                 celdas[0].innerHTML = turnPlace();
                 playerCpu.push(0);
-                console.log("3")
                 throw cpuPlayed;
             }else if(celdas[2].innerHTML === ""){
                 celdas[2].innerHTML = turnPlace();
                 playerCpu.push(2);
-                console.log("4")
                 throw cpuPlayed;
             }else if(celdas[6].innerHTML === ""){
                 celdas[6].innerHTML = turnPlace();
                 playerCpu.push(6);
-                console.log("5")
                 throw cpuPlayed;
             }else if(celdas[8].innerHTML === ""){
                 celdas[8].innerHTML = turnPlace();
                 playerCpu.push(8);
-                console.log("6")
                 throw cpuPlayed;
             }
         }
-        /* Que busque una ruta para ganar en la que el jugador no posea "puntos"*/
+        /* Que busque una ruta para ganar en la que el jugador no posea "marcas"*/
         winCondition.forEach(arr=>{
             if(playerOne.every(elem=>{
                 return !arr.includes(elem);
@@ -166,22 +149,218 @@ const cpu = ()=>{
         
     }
     catch(cpuPlayed){}
+    finally{
+        if(gameCover==="long"){
+            $(".game__gamegrid").addClass("game__cover");
+            setTimeout(()=>{
+                $(".game__gamegrid").removeClass("game__cover");
+                anyWon();
+            },3001)
+        }else{
+            $(".game__gamegrid").addClass("game__cover");
+            setTimeout(()=>{
+                $(".game__gamegrid").removeClass("game__cover");
+                anyWon();
+            },601)
+        }
+        
+    }
 }
-
-//RESTART BUTTON
-document.querySelector(".game__restart").addEventListener("click",()=>{
-    celdas.forEach(elem=>{
+//Re establece el juego
+const gameRestart = ()=>{
+    celdas.forEach(elem=>{  
         elem.innerHTML = ""
-    })
+    });
     playerOne.length=0;
     playerTwo.length=0;
     playerCpu.length=0;
     turnCount = 1;
-    if(turnContainer==="o"){
+    if(turnContainer==="o" && rivalSelect === "cpu"){
         turnCount=3;
         cpu();
+    }else if(turnContainer==="o"){
+        turnCount=2;
     }
+    turnIndicator(turnCount);
+}
+//Pregunta de cambiar marca
+const changeMarkWindow = ()=>{
+    if(dama===false){
+        if($("#counter__x").html() != "0" || $("#counter__o").html() != "0" || $("#counter__ties").html() != "0"){
+            $(".windows").css({
+                "display" : "flex"
+            })
+            $(".change").css({
+                "display" : "flex"
+            })
+        }
+        else{
+            turnContainer = turnContainerAux;
+            gameRestart();
+        }
+    }else{
+        turnContainer = turnContainerAux;
+        scoreInZero();
+    }
+
+}
+//Score en 0
+const scoreInZero = ()=>{
+    $("#counter__x").html("0");
+    $("#counter__o").html("0");
+    $("#counter__ties").html("0");
+    gameRestart();
+}
+//Indicador de turnos
+const turnIndicator = (turnIndicatorAux)=>{
+    ((turnIndicatorAux+1) % 2 === 0) ? 
+        document.querySelector(".game__turn").id="x" : 
+        document.querySelector(".game__turn").id="o";
+}
+//Pregunta si gano alguien
+const anyWon = ()=>{
+    let anyWonAux = true;
+    let celdasAux = [...celdas];
+    try {
+        winCondition.forEach(arr=>{
+            if(arr.every(elem=>playerOne.includes(elem))){
+                anyWonAux = winnerWindow(turnContainer,arr);
+                throw err;
+            }else if(arr.every(elem=>playerTwo.includes(elem))){
+                anyWonAux = turnContainer==="o" ? 
+                    winnerWindow("x",arr) : 
+                    winnerWindow("o",arr);
+                    throw err;
+            }else if(arr.every(elem=>playerCpu.includes(elem))){
+                anyWonAux = turnContainer==="o" ? 
+                    winnerWindow("x",arr) : 
+                    winnerWindow("o",arr);
+                    throw err;
+            }
+        })
+        if(anyWonAux != false && celdasAux.every(elem=>elem.innerHTML !== "")){
+            anyWonAux = winnerWindow("ties");
+        }
+    } catch (err) {}finally{
+        return anyWonAux;
+    }
+}
+//Cartel de victoria
+const winnerWindow = (winner,arr)=>{
+    document.querySelector(".win__paragraph").innerHTML = "YOU WON!";
+    document.querySelector(".win__whowon-text").innerHTML = "TAKES THE ROUND";
+    document.querySelector(".win__whowon-mark").id = winner;
+    if(winner==="o"){
+        $(".win__whowon-text").css({
+            "color": "var(--o)"
+        })
+    }else if(winner==="x"){
+        $(".win__whowon-text").css({
+            "color": "var(--x)"
+        })
+    }else{
+        $(".win__whowon-text").css({
+            "color": "var(--text)"
+        })
+        document.querySelector(".win__paragraph").innerHTML = "NO ONE WIN"
+        document.querySelector(".win__whowon-text").innerHTML = "TIE!"
+    }
+    if(turnContainer != winner && winner != "ties"){
+        document.querySelector(".win__paragraph").innerHTML = "YOU LOSE!"
+    }
+    if(rivalSelect==="cpu" && enTuCara==="yes"){
+        document.querySelector(".win__paragraph").innerHTML = "TIC TAC TOE"
+        document.querySelector(".win__whowon-text").innerHTML = "¡EN TU CARA!"
+        enTuCara="no"
+    }
+    if(arr){
+        arr.forEach(elem=>{
+            $(`#${elem}`).css({
+                "filter": "brightness(1.5)"
+            })
+            
+        })
+    }
+    $(".game__gamegrid").addClass("game__cover");
+    setTimeout(()=>{
+        $(".windows").css({
+            "display": "flex"
+        });
+        $(".win").css({
+            "display": "flex"
+        });
+        $(".game__gamegrid").removeClass("game__cover");
+        if(arr){
+            arr.forEach(elem=>{
+                $(`#${elem}`).css({
+                    "filter": "brightness(1)"
+                })
+                
+            })
+        }
+        scoreCount(winner);
+        return false;
+    },1000)
+    
+    
+}
+//Contador de puntos
+const scoreCount = (winner)=>{
+    $(`#counter__${winner}`).html(Number($(`#counter__${winner}`).html())+1);
+}
+//Cerrar cartel de victoria
+const winnerWindowClose = ()=>{
+    $(".windows").css({
+        "display": "none"
+    });
+    $(".win").css({
+        "display": "none"
+    });
+}
+//Ingresar al juego
+const joinToGame = ()=>{
+    $(".menu").css({
+        "margin-left": "-375px"
+    })
+    scoreInZero();
+}
+//-----------------------------------Eventos de clicks-----------------------------------
+//Colocar marcas
+celdas.forEach(elem=>{
+    elem.addEventListener("click",()=>{
+        if(elem.innerHTML === ""){
+            elem.innerHTML = turnPlace();
+            elem.innerHTML == `<div class="animacion-${turnContainer}" id="${turnContainer}"></div>` ? 
+                playerOne.push(Number(elem.id)) : 
+                playerTwo.push(Number(elem.id));
+            $(".game__gamegrid").addClass("game__cover");
+            setTimeout(()=>{
+                $(".game__gamegrid").removeClass("game__cover");
+                if(anyWon() && rivalSelect==="cpu"){
+                    cpu();
+                }
+            },650)
+            
+        }
+    })
 })
+//RESTART BUTTON
+document.querySelector(".game__restart").addEventListener("click",gameRestart)
+//Visibilidad de ventanas Menu y Game || Jugador contra jugador o jugador contra maquina btn
+$("#vs__p").click(()=>{
+    rivalSelect = "player";
+    joinToGame();
+});
+$("#vs__cpu").click(()=>{
+    rivalSelect = "cpu";
+    joinToGame();
+});
+document.querySelector("#gobacktomenu").addEventListener("click",()=>{
+    $(".menu").css({
+        "margin-left": "0"
+    });
+    scoreInZero();
+});
 //Seleccionar la marca del primer player
 document.querySelectorAll(".ttt__select-btn").forEach(elem=>{
     elem.addEventListener("click",()=>{
@@ -192,39 +371,45 @@ document.querySelectorAll(".ttt__select-btn").forEach(elem=>{
         document.querySelector(".select-btn-active").disabled = false;
         document.querySelector(".uneffect-btn").disabled = true;
     })
-    
 })
-
-//Visibilidad de ventanas
-document.querySelectorAll(".ttt__btn").forEach(elem=>{
-    elem.addEventListener("click",()=>{
-        $(".game").css({
-            "visibility": "visible"
-        });
-        $(".menu").css({
-            "visibility": "hidden"
-        });
-    });
-});
-document.querySelector("#gobacktomenu").addEventListener("click",elem=>{
-    $(".game").css({
-        "visibility": "hidden"
-    });
+//Cerrar Cartel de victoria o volver al menu
+document.querySelector("#win__btn-quit").addEventListener("click",()=>{
+    winnerWindowClose();
     $(".menu").css({
-        "visibility": "visible"
+        "margin-left": "0"
     });
-});
-// .ttt__select-btn:nth-of-type(1){
-//     background: none;
-//     box-shadow: none;
-// }
-// .ttt__select-btn > #x::after, .ttt__select-btn > #x::before {
-//     background: var(--text);
-// }
-// .ttt__select-btn:nth-of-type(2){
-//     background: var(--text);
-//     box-shadow: 0px 3px 0px var(--text__shadow);
-// }
-// .ttt__select-btn > #o {
-//     border-color: var(--background);
-// }
+    gameRestart();
+})
+document.querySelector("#win__btn-nextround").addEventListener("click",()=>{
+    winnerWindowClose();
+    gameRestart();
+})
+//Ventana de advertencia de cambio de marca
+document.querySelectorAll(".turn").forEach(elem=>{
+    elem.addEventListener("click",()=>{
+        turnContainerAux = elem.id;
+        changeMarkWindow();
+    })
+})
+$("#change__btn-accept").click(()=>{
+    $(".windows").css({
+        "display":"none"
+    });
+    $(".change").css({
+        "display":"none"
+    });
+    turnContainer = turnContainerAux;
+    scoreInZero();
+})
+$("#change__btn-cancel").click(()=>{
+    $(".windows").css({
+        "display":"none"
+    });
+    $(".change").css({
+        "display":"none"
+    });
+})
+//Dont ask me again de la ventana de cambio
+$("#change__dama").click(()=>{
+    dama = dama===true ? false : true;
+})
---
+++ proyectos/Admin-AT/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header class="header">
+        <div class="autodidactas-logo">
+            <img src="" alt="" class="autodidactas__img">
+            <p class="autodidactas__nombre">Autodidactas<br>Team</p>
+        </div>
+        <nav class="nav">
+            <button class="nav__btn">Ver miembro</button>
+            <button class="nav__btn">Añadir miembro</button>    
+        </nav>
+    </header>
+    <main class="main">
+        <form action="" method="" class="form">
+            <div class="form__fila-1">
+                
+            </div>
+        </form>
+    </main>
+</body>
+</html>
